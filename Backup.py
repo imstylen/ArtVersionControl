c7a432e41656c340ecf5978100ac37e2ad714ead
imstylen <michael.steijlen@gmail.com>
2020-05-17 16:21:04 -0400

    broken

--- Backup.py
+++ Backup.py
@@ -1,8 +1,9 @@
 import time
 import os
 from shutil import copy2
+import datetime
 
-def backup(dir_path, root_path):
+def commit_to_master(dir_path, root_path, now_time):
 
     items = os.listdir(dir_path)
 
@@ -24,16 +25,16 @@ def backup(dir_path, root_path):
     for folder in project_folders:
         if folder != "BACKUP":
 
-            backup_dir = root_path + "\\" + "BACKUP" + dir_path[len(root_path):] + folder + "\\"
+            backup_dir = root_path + "\\" + "BACKUP" +"\\" + now_time + dir_path[len(root_path):] + folder + "\\"
 
             if os.path.exists(backup_dir) == False:
                 os.mkdir(backup_dir)
 
-            backup(dir_path + folder,root_path)
+            commit_to_master(dir_path + folder,root_path, now_time)
 
     for file in project_files:
         if file != "Backup.py" and file != "Backup.bat":
-            backup_path = root_path + "\\" + "BACKUP" + dir_path[len(root_path):]
+            backup_path = root_path + "\\" + "BACKUP" + "\\" + now_time + dir_path[len(root_path):]
             src = dir_path + file
 
             current_revision = 0
@@ -61,8 +62,8 @@ if __name__ == '__main__':
 
     print("Performing backup on all modified project files.")
     start_time = time.time()
-    root_path = os.path.dirname(os.path.realpath(__file__))
-
-    backup(root_path,root_path)
+    root_path = os.path.dirname(os.path.realpath(__file__)) + "\\" + "Development"
+    now_time = datetime.datetime.now().strftime("%m_%d_%Y %a %I-%M-%S %p")
+    commit_to_master(root_path,root_path,now_time)
 
     print("---- Backup completed in %.3f seconds ---" %(time.time() - start_time))
---
+++ test.py
@@ -0,0 +1,5 @@
+import datetime
+
+x = datetime.datetime.now().strftime("%m_%d_%Y %a %I-%M-%S %p")
+
+print(x)
